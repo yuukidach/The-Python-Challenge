da678b2c4324fb86cf33544d7327309e50fdfcac
yuukidach <chendamailbox@foxmail.com>
2016-09-17 13:42:10 +0800

    change leve1.py

--- level1.py
+++ level1.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 code = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dm"
-        "p. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq "        "qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc"        " spj.")
+        "p. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq "
+        "qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc"
+        " spj.")
 addr = ""
 
 for alpha in code:

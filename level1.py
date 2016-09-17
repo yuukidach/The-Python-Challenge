2c04686b2ad442a6c81110a02f2180471d0f5519
yuukidach <chendamailbox@foxmail.com>
2016-09-17 13:40:07 +0800

    first commit

--- level1.py
+++ level1.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-code = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+code = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dm"
+        "p. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq "        "qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc"        " spj.")
 addr = ""
 
 for alpha in code:
--- level1[v2].py
+++ level1[v2].py
@@ -4,7 +4,7 @@
 code = ("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dm"
         "p. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq "
         "qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc"
-        "spj.")
+        " spj.")
 
 intab = "abcdefghijklmnopqrstuvwxyz"
 outtab = "cdefghijklmnopqrstuvwxyzab"
